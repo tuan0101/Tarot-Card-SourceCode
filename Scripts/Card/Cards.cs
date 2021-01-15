45c04e0078f11ba8f350e080d88db6efc02b24b1
Tuan <60296627+tuan0101@users.noreply.github.com>
2021-01-15 14:01:45 -0500

    Update Cards.cs

--- Scripts/Card/Cards.cs
+++ Scripts/Card/Cards.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [CreateAssetMenu(menuName = "Cards")]
 public class Cards : ScriptableObject
 {
-
     public int cNumber;
     public string cName;
 }
