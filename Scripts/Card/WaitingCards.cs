75f5b1066b025de17a7a3d645d27f3e16c227393
Tuan Pham <tuan.programmer0101@gmail.com>
2021-01-15 14:26:32 -0500

    Added WaitingCards.cs

---
+++ Scripts/Card/WaitingCards.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitingCards : MonoBehaviour
+{
+    public static List<int> indexList = null;
+
+    public void Awake()
+    {
+        //DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        this.gameObject.GetComponent<WaitingCards>();
+        indexList = new List<int>();
+    }
+
+    public void setList(int value)
+    {
+        indexList.Add(value);
+    }
+
+}
